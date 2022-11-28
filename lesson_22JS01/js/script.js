ec63294cf5737e158074ee10b35be1f589c9ced4
Kolia27 <70968383+Kolia27@users.noreply.github.com>
2022-11-28 12:02:41 +0200

    Add files via upload

---
+++ lesson_22JS01/js/script.js
@@ -0,0 +1,15 @@
+// Строгий режим
+"use strict"
+
+console.log('Я вчу JS');
+
+// 1
+let color_Eyes;
+color_Eyes = 'green';
+console.log(color_Eyes);
+// 2
+let user, userName;
+userName = 'Вася';
+user = userName;
+console.log(user);
+
